4b3095a01eaea2b5cdec5c4c94abea5aa5bb9830
nicolas2289h <nicolas2289h@gmail.com>
2026-02-12 23:57:56 -0300

    uploading springboot initial project

---
+++ backend/src/test/java/com/tesis/demo/TesisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
