1098c98f5dac7d79a564c1f29f249e698e63470f
nicolas2289h <nicolas2289h@gmail.com>
2026-07-12 23:22:10 -0300

    se agregan supermercados Dia y Vea en listado de scrapers

--- scraper/src/scrapers/__init__.py
+++ scraper/src/scrapers/__init__.py
@@ -1,10 +1,14 @@
 from ..base_scraper import BaseScraper
 from .generic_jsonld_scraper import GenericJsonLdScraper
 from .scraper_factory import ScraperFactory
+from .dia_scraper import DiaScraper
+from .vea_scraper import VeaScraper
 
 __all__ = [
     "BaseScraper",
     "GenericJsonLdScraper",
-    "ScraperFactory"
+    "ScraperFactory",
+    "DiaScraper",
+    "VeaScraper",
 ]
 
